2f0ecd4b48761b31c893b4eaa28eae19281560d2
wangdi <1490403440@qq.com>
2017-08-15 21:08:38 +0800

    暂存

--- lego/contract_sale/contract_sale_all_penle.js
+++ lego/contract_sale/contract_sale_all_penle.js
@@ -3,6 +3,8 @@ var contract_sale_all_penle =
     '<td colspan="11">'+
       '<div class="row">'+
         '<div class="col-lg-12">'+
+          '<div id = "paid_record_content">'+
+          '</div>'+
           '<div id = "contract_logistics_content">'+
           '</div>'+
         '</div>'+
