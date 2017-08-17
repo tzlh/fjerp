6c348fa267589b741678daee2eef9986f77d569b
yuanhang <396060520@qq.com>
2017-08-17 10:53:08 +0800

    修改了登录和demo

--- lego/demo/demo.js
+++ lego/demo/demo.js
@@ -2,13 +2,18 @@ function DemoObject(name, age, job) {
     this.name = name;
     this.age = age;
     this.job = job;
+    this.alert_msg = function() {
+      alert("kkkkkkkkkkkk");
+    };
     this.setAge = function(age) {
-      alert(this.age + age);
-    }
+      alert(this.name);
+      // this.alert_msg();
+      // alert(this.age + age);
+    };
     this.getName = function() {
         return this.name;
-    }
+    };
     this.getJob = function(sig) {
         return "[" + sig + "]" + this.job;
-    }
+    };
 }
