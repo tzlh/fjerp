a3f7e8e808f4090ef38d6427e501705450d612e3
yuanhang <396060520@qq.com>
2017-09-08 18:32:17 +0800

    更新了生产模块的接口

--- js/configure.js
+++ js/configure.js
@@ -3,5 +3,5 @@
  */
 
 // var PROJECT_PATH = "http://192.168.1.131:8080/lego/";
-var PROJECT_PATH = "http://127.0.0.1:8080/lego/";
-// var PROJECT_PATH = "http://47.92.152.242:8080/lego/";
+// var PROJECT_PATH = "http://127.0.0.1:8080/lego/";
+var PROJECT_PATH = "http://47.92.152.242:8080/lego/";
