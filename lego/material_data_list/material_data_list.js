efb56f6d902654882abf855c531ce9102a098bea
wangdi <1490403440@qq.com>
2017-09-09 15:58:49 +0800

    类别列表数据库完成

--- lego/material_data_list/material_data_list.js
+++ lego/material_data_list/material_data_list.js
@@ -14,9 +14,9 @@ class MaterialDataListObj {
     // 类别子集
     this.materialDataListSubData = {
     "data":[
-        {"material_sub_name":"芳烃", "weight":"300", "percentage":"30", "uuid":"101", "material_uuid":"001"},
-        {"material_sub_name":"异构", "weight":"300", "percentage":"30", "uuid":"102", "material_uuid":"001"},
-        {"material_sub_name":"轻油", "weight":"300", "percentage":"30", "uuid":"103", "material_uuid":"002"}]
+        {"material_sub_name":"芳烃", "weight":"300", "price":"3000", "uuid":"101", "material_uuid":"001"},
+        {"material_sub_name":"异构", "weight":"300", "price":"3000", "uuid":"102", "material_uuid":"001"},
+        {"material_sub_name":"轻油", "weight":"300", "price":"3000", "uuid":"103", "material_uuid":"002"}]
     };
   } 
 
@@ -25,19 +25,49 @@ class MaterialDataListObj {
     $("#material_data_list_box").html("");
   }
 
-  //填充复该数据
+  //填充覆盖数据
   fillVariableData() {
     //查询列表
     let materialDataListHtml = "";
     if(isJsonObjectHasData(this.materialDataListData)) {
       for (let i = 0; i < this.materialDataListData.data.length; i++) {
-        materialDataListHtml+=
-          `<div>
-            <p href = "#" class = "list-group-item clearfix material_data_list_pl30 material_data_list_bgddd">${this.materialDataListData.data[i].material_name}</p>
-            <div id = "material_data_list${this.materialDataListData.data[i].uuid}">
+        let weightAll = 0;
+        materialDataListHtml +=
+          `<div class = "warehouse_content_all">
+            <p class = "list-group-item clearfix material_data_list_pl30 material_data_list_bgddd">${this.materialDataListData.data[i].material_name}</p>
+            <div id = "material_data_list">
               <table class = "table table-bordered">
-                <tbody id = "material_body_content">
-                </tbody>
+                <tbody id = "material_body_content" class = "pot_content_body">`;
+                if(isJsonObjectHasData(this.materialDataListSubData)) {
+                  for (let j = 0; j < this.materialDataListSubData.data.length; j++) {
+                    if(this.materialDataListData.data[i].uuid == this.materialDataListSubData.data[j].material_uuid) {
+                      weightAll += Number(this.materialDataListSubData.data[j].weight);
+                    }
+                  }
+                  for (let j = 0; j < this.materialDataListSubData.data.length; j++) {
+                    if(this.materialDataListData.data[i].uuid == this.materialDataListSubData.data[j].material_uuid) {
+                      let weight = 0;
+                      let price = 0;
+                      let percentage = 0;
+                      if("" != this.materialDataListSubData.data[j].weight || "0" != this.materialDataListSubData.data[j].weight) {
+                        weight = this.materialDataListSubData.data[j].weight;
+                        percentage = ((this.materialDataListSubData.data[j].weight) / weightAll).toFixed(2);
+                      }
+                      if("" != this.materialDataListSubData.data[j].price) {
+                        price = this.materialDataListSubData.data[j].price;
+                      }
+                      materialDataListHtml += 
+                        `<tr>
+                           <td width = "40%">${this.materialDataListSubData.data[j].material_sub_name}</td>
+                           <td width = "20%">${weight}</td>
+                           <td width = "20%">￥${price}</td>
+                           <td width = "20%">${percentage}%</td>
+                         </tr>`;
+                    } 
+                  }
+                }
+                materialDataListHtml +=
+               `</tbody>
               </table>
             </div>
           </div>`;
@@ -45,15 +75,130 @@ class MaterialDataListObj {
     }
     $("#material_data_list_box").html(materialDataListHtml);
     //查询列表子集
-    if(isJsonObjectHasData(this.materialDataListSubData)) {
-      for (let i = 0; i < this.materialDataListSubData.data.length; i++) {
-        $("#material_data_list" + this.materialDataListSubData.data[i].material_uuid).find("#material_body_content").append(`
-          <tr>
-            <td>${this.materialDataListSubData.data[i].material_sub_name}</td>
-            <td>${this.materialDataListSubData.data[i].weight}</td>
-            <td>${this.materialDataListSubData.data[i].percentage}%</td>
-          </tr>`);
+  }
+
+  //服务器数据覆盖
+  serverDataCover() {
+    this.materialDataListData = {};
+    this.materialDataListSubData = {};
+    //获取仓库
+    let warehouseUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehouse";
+    let warehouseGet = ajax_assistant(warehouseUrl, "", false, true, false);
+    //获取储罐原料类别
+    let warehouseTypeUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePotMaterialType";
+    let warehouseTypeGet = ajax_assistant(warehouseTypeUrl, "", false, true, false);
+    //获取储罐
+    let potUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePot";
+    let potGet = ajax_assistant(potUrl, "", false, true, false);
+    //仓库
+    if ("1" == warehouseGet.status) {
+      if ("0" == warehouseGet.count) {
+        this.materialDataListData = {};
+      } else {
+        let tmpArr = new Array();
+        let result = JSON.parse(warehouseGet.result);    
+        for (let i = 0; i < result.length; i++) {
+          tmpArr[i] = {"material_name":result[i].name, "uuid":result[i].uuid};
+        }
+        this.materialDataListData["data"] = tmpArr;
       }
+    } else {
+      alert("仓库数据获取失败");
     }
+    //获取储罐原料类别
+    if ("1" == warehouseTypeGet.status) {
+      if ("0" == warehouseTypeGet.count) {
+        this.materialDataListSubData = {};
+      } else {
+        let tmpArr = new Array();
+        let result = JSON.parse(warehouseTypeGet.result);    
+        for (let i = 0; i < result.length; i++) {
+          tmpArr[i] = {"material_sub_name":result[i].name, "weight":"2", "price":"", "uuid":result[i].uuid};
+          if("1" == potGet.status) {
+            let potResult = JSON.parse(potGet.result);
+            for(let j = 0; j < potResult.length; j++) {
+              if (potResult[j].uuid == result[i].pot_uuid) {
+                tmpArr[i]["material_uuid"] = potResult[j].warehouse_uuid; 
+              }
+            }
+          } else {
+            alert("获取储罐失败！");
+          }
+        }
+        this.materialDataListSubData["data"] = tmpArr;
+      }
+    } else {
+      alert("储罐原料类别数据获取失败");
+    }
+  }
+
+  //输出文本
+  outPutMaterialContent(contentBoxId) {
+    let content = 
+      `<div class = "row material_data_list_row">
+         <div class = "col-lg-12 material_data_list_col">
+           <div class = "list-group">
+             <p href = "#" class = "list-group-item clearfix active">数据汇总</p>
+             <div id="material_data_list_box">
+               <div>
+                 <p class = "list-group-item clearfix material_data_list_pl30 material_data_list_bgddd">舟山纳海</p>
+                 <div>
+                   <table class = "table table-bordered">
+                     <tbody>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                     </tbody>
+                   </table>
+                 </div>
+               </div>
+               <div>
+                 <p class = "list-group-item clearfix material_data_list_pl30 material_data_list_bgddd">舟山纳海</p>
+                 <div>
+                   <table class = "table table-bordered">
+                     <tbody>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                       <tr>
+                         <td width = "40%">芳烃</td>
+                         <td width = "20%">300吨</td>
+                         <td width = "20%">￥200</td>
+                         <td width = "20%">30%</td>
+                       </tr>
+                     </tbody>
+                   </table>
+                 </div>
+               </div>
+             </div>
+             <p class="list-group-item active text-center">&nbsp;</p>
+           </div>
+         </div>
+       </div>`;
+    $(contentBoxId).html(content);
   }
 }
--- lego/material_type_list/material_type_list.js
+++ lego/material_type_list/material_type_list.js
@@ -25,7 +25,7 @@ class MaterialListObj {
     $("#material_type_list_box").html("");
   }
 
-  //填充复该数据
+  //填充覆盖数据
   fillVariableData() {
     //查询列表
     let materialListHtml = "";
@@ -33,17 +33,76 @@ class MaterialListObj {
       for (let i = 0; i < this.materialListData.data.length; i++) {
         materialListHtml+=
           `<div>
-            <p href = "#" class = "list-group-item clearfix material_type_list_pl30 material_type_list_bgddd">${this.materialListData.data[i].material_name}<span class = "glyphicon glyphicon-plus pull-right material_type_list_colorfff add_material_plus"></span></p>
+            <p href = "#" class = "list-group-item clearfix material_type_list_pl30 material_type_list_bgddd">${this.materialListData.data[i].material_name}<span class = "glyphicon glyphicon-plus pull-right material_type_list_colorfff add_material_plus" warehouse_uuid = "${this.materialListData.data[i].uuid}"></span></p>
             <div id = "material_type_list${this.materialListData.data[i].uuid}"></div>
           </div>`;
       }
     }
     $("#material_type_list_box").html(materialListHtml);
     //查询列表子集
-    if(isJsonObjectHasData(this.materialListSubData)) {
+    if (isJsonObjectHasData(this.materialListSubData)) {
       for (let i = 0; i < this.materialListSubData.data.length; i++) {
         $("#material_type_list" + this.materialListSubData.data[i].material_uuid).append(`<p href = "#" class = "list-group-item clearfix material_type_list_pl30">${this.materialListSubData.data[i].material_sub_name}<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15 delete_material_event" uuid = "${this.materialListSubData.data[i].uuid}"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15 material_edit_event" uuid = "${this.materialListSubData.data[i].uuid}"></span></p>`);
       }
+    }
+  }
+  
+  //数据库数据覆盖
+  serverDataCover() {
+    this.materialListData = {};
+    this.materialListSubData = {};
+    //获取仓库
+    let warehouseUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehouse";
+    let warehouseGet = ajax_assistant(warehouseUrl, "", false, true, false);
+    //获取储罐原料类别
+    let warehouseTypeUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePotMaterialType";
+    let warehouseTypeGet = ajax_assistant(warehouseTypeUrl, "", false, true, false);
+    //获取储罐
+    let potUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePot";
+    let potGet = ajax_assistant(potUrl, "", false, true, false);
+    console.log(warehouseGet)
+    console.log(warehouseTypeGet);
+    //仓库
+    if (1 == warehouseGet.status) {
+      if (0 == warehouseGet.count) {
+        this.materialListData = {};
+      } else {
+        let tmpArr = new Array();
+        let result = JSON.parse(warehouseGet.result);    
+        for (let i = 0; i < result.length; i++) {
+          tmpArr[i] = {"material_name":result[i].name, "uuid":result[i].uuid};
+        }
+        this.materialListData["data"] = tmpArr;
+        console.log(tmpArr)
+      }
+    } else {
+      alert("仓库数据获取失败");
+    }
+    //获取储罐原料类别
+    if ("1" == warehouseTypeGet.status) {
+      if (0 == warehouseTypeGet.count) {
+        this.materialListSubData = {};
+      } else {
+        let tmpArr = new Array();
+        let result = JSON.parse(warehouseTypeGet.result);    
+        for (let i = 0; i < result.length; i++) {
+          tmpArr[i] = {"material_sub_name":result[i].name, "uuid":result[i].uuid};
+          if ("1" == potGet.status) {
+            let potResult = JSON.parse(potGet.result);
+            for (let j = 0; j < potResult.length; j++) {
+              if (potResult[j].uuid == result[i].pot_uuid) {
+                tmpArr[i]["material_uuid"] = potResult[j].warehouse_uuid; 
+              }
+            }
+          } else {
+            alert("获取储罐失败！");
+          }
+        }
+        this.materialListSubData["data"] = tmpArr;
+        console.log(tmpArr)
+      }
+    } else {
+      alert("储罐原料类别数据获取失败");
     }
   }
 
@@ -77,7 +136,24 @@ class MaterialListObj {
   }
 
   //添加类别列表
-  addModal() {
+  addModal(obj) {
+    let warehouseUuid = obj.attr("warehouse_uuid");
+    //获取该仓库的储罐
+    let potData = {
+      "warehouse_uuid":warehouseUuid
+    };
+    let potUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePot";
+    let potGet = ajax_assistant(potUrl, potData, false, true, false);
+    let potHtml = `<option value = "">--请选择--</option>`;
+    if ("1" == potGet.status) {
+      let potResult = JSON.parse(potGet.result);
+      for (let i = 0; i < potResult.length; i++) {
+        potHtml += `<option value = "${potResult[i].uuid}">${potResult[i].name}</option>`;
+      }
+    } else {
+      alert("获取储罐失败！");
+    }
+    //弹框文本
     let materialHtml  =  
       `<div class = "modal fade custom_modal custom_modal" id = "material_type_list_prop" tabindex = "-1" role = "dialog" aria-labelledby = "myModalLabel">
         <div class = "modal-dialog modal-sm" role = "document">
@@ -87,6 +163,11 @@ class MaterialListObj {
               <h4 class = "modal-title" id = "myModalLabel">添加类别</h4>
             </div>
             <div class = "modal-body nopadding-bottom">
+              <div class = "form-group">
+                <label>选择储罐</label>
+                <select class = "form-control pot_name" value = "">${potHtml}
+                </select>
+              </div>
               <div class = "form-group">
                 <label>类别名称</label>
                 <input type = "text" class = "form-control material_list_name" value = "">
@@ -108,31 +189,36 @@ class MaterialListObj {
 
   //添加数据
   addData(obj) {
+    let potUuid = obj.parents("#material_type_list_prop").find(".pot_name").val();
     let materialListName = obj.parents("#material_type_list_prop").find(".material_list_name").val();
     if(null == materialListName.match(/^[\u4e00-\u9fffa0-9a-zA-Z]{1,32}$/)) {
       alert("请输入正确的类别名称");
       return;
     }
+    //判断原料类别是否重名
+    let materialCheckData = {
+      "name":materialListName
+    };
+    let materialCheckUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=checkWarehousePotMaterialType";
+    let materialCheckGet = ajax_assistant(materialCheckUrl, materialCheckData, false, true, false);
+    if("1" != materialCheckGet.status) {
+      alert("改类别名已存在！");
+      return;
+    }
     let materialAddData = {
+      "pot_uuid":potUuid,
       "name":materialListName
     };
     // 调用后台添加接口
-    //let materialAddUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=addWarehouse";
-    //let materialAddGet = ajax_assistant(materialAddUrl, materialAddData, false, true, false);
-    //if (1 != materialAddGet.status) {
-    if ("abc" != materialListName) {
+    let materialAddUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=addWarehousePotMaterialType";
+    let materialAddGet = ajax_assistant(materialAddUrl, materialAddData, false, true, false);
+    console.log(materialAddGet)
+    if (1 != materialAddGet.status) {
       alert("添加类别名称失败");
     } else {
-    // 更新页面数据
+      //更新页面数据
       this.clearRawData();
-      //serverDataCover();
-      this.materialListSubData = {
-        "data":[
-            {"material_sub_name":"芳烃", "uuid":"101", "material_uuid":"001"},
-            {"material_sub_name":"芳烃1", "uuid":"101", "material_uuid":"001"},
-            {"material_sub_name":"异构", "uuid":"102", "material_uuid":"001"},
-            {"material_sub_name":"轻油", "uuid":"103", "material_uuid":"002"}]
-        }
+      this.serverDataCover();
       this.fillVariableData(); 
       $("#material_type_list_prop").modal("hide");
       $("#material_type_list_prop").on("hidden.bs.modal", function (e) {
@@ -144,16 +230,37 @@ class MaterialListObj {
   //修改类别列表
   editModal(obj) {
     let materialUuid = obj.attr("uuid");
+    //获取类别
     let data = {
       "uuid":materialUuid
     };
-    let materialName = "好的";
-    //let editUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehouse";
-    //let editGet = ajax_assistant(editUrl, data, false, true, false);
-    //if(1 == editGet.status){
-    //if("001" == editGet.status){
-      //let reslutJson = JSON.parse(editGet.result);
-    //}
+    let materialName = "";
+    let potCheckUuid = "";
+    let editUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePotMaterialType";
+    let editGet = ajax_assistant(editUrl, data, false, true, false);
+    if(1 == editGet.status){
+      let reslutJson = JSON.parse(editGet.result);
+      materialName = reslutJson[0].name;
+      potCheckUuid = reslutJson[0].pot_uuid;
+    } else {
+      alert("获取储罐原料类别失败！");
+    }
+    //获取储罐
+    let potUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePot";
+    let potGet = ajax_assistant(potUrl, "", false, true, false);
+    let potHtml = "";
+    if("1" == potGet.status) {
+      let potResult = JSON.parse(potGet.result);
+      for(let i = 0; i < potResult.length; i++) {
+        if (potCheckUuid == potResult[i].uuid) {
+          potHtml += `<option value = "${potResult[i].uuid}">${potResult[i].name}</option>`;
+ 
+        }
+      }
+    } else {
+      alert("获取储罐失败！");
+    }
+
     let materialHtml = 
      `<div class = "modal fade custom_modal" id = "material_type_list_prop_edit" tabindex = "-1" role = "dialog" aria-labelledby = "myModalLabel">
       <div class = "modal-dialog modal-sm" role = "document">
@@ -164,7 +271,13 @@ class MaterialListObj {
           </div>
           <div class = "modal-body nopadding-bottom">
             <div class = "form-group">
+              <label>选择储罐</label>
+              <select class = "form-control pot_name" value = "" disabled = "disabled">${potHtml}
+              </select>
+            </div>
+            <div class = "form-group">
               <label>类别名称</label>
+              <input type="hidden" class = "name_hidden" value="${materialName}">
               <input type = "text" class = "form-control material_list_name" value = "${materialName}">
             </div>
           </div>
@@ -190,27 +303,35 @@ class MaterialListObj {
       alert("请输入正确的类别名称");
       return;
     };
+    //判断原料类别是否重名
+    let materialCheckData = {
+      "name":materialName
+    };
+    let materialCheckUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=checkWarehousePotMaterialType";
+    let materialCheckGet = ajax_assistant(materialCheckUrl, materialCheckData, false, true, false);
+    let vleHidden = obj.parents("#material_type_list_prop_edit").find(".name_hidden").val();
+    if("1" != materialCheckGet.status) {
+      console.log(vleHidden);
+      console.log(materialName);
+      if (vleHidden != materialName) {
+        alert("改类别名已存在！");
+        return;
+      }
+    }
     let editData = {
       "uuid":materialUuid,
       "name":materialName
     };
     // 调用后台添加接口
-    //let editUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=modifyWarehouse";
-    //let editGet = ajax_assistant(editUrl, editData, false, true, false);
-    //console.log(warehouse_edit_get_warehouse);
-    //if (1 != editGet.status) {
-    if ("abc" != materialName) {
+    let editUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=modifyWarehousePotMaterialType";
+    let editGet = ajax_assistant(editUrl, editData, false, true, false);
+    console.log(editGet);
+    if ("1" != editGet.status) {
       alert("修改类别名称失败");
     } else {    
-    // 更新页面数据
+      //更新页面数据
       this.clearRawData();
-      //this.serverDataCover();
-      this.materialListSubData = {
-        "data":[
-            {"material_sub_name":"abc", "uuid":"101", "material_uuid":"001"},
-            {"material_sub_name":"异构", "uuid":"102", "material_uuid":"001"},
-            {"material_sub_name":"轻油", "uuid":"103", "material_uuid":"002"}]
-        }
+      this.serverDataCover();
       this.fillVariableData();
       $("#material_type_list_prop_edit").modal("hide");
       $("#material_type_list_prop_edit").on("hidden.bs.modal", function (e) {
@@ -251,20 +372,14 @@ class MaterialListObj {
     let data = {
       "uuid":uuid
     };
-    let deleteUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=removeWarehouse";
+    let deleteUrl = PROJECT_PATH + "lego/lego_fjTrade?servletName=removeWarehousePotMaterialType";
     let deleteGet = ajax_assistant(deleteUrl, data, false, true, false);
-    //if (1 != deleteGet.status) {
-    if ("101" != uuid) { 
+    if ("1" != deleteGet.status) {
       alert("删除仓库失败");
     } else {    
     // 更新页面数据
       this.clearRawData();
-      //this.serverDataCover();
-      this.materialListSubData = {
-        "data":[
-            {"material_sub_name":"异构", "uuid":"102", "material_uuid":"001"},
-            {"material_sub_name":"轻油", "uuid":"103", "material_uuid":"002"}]
-        }
+      this.serverDataCover();
       this.fillVariableData();
     }
     $("#material_type_list_delet").modal("hide");
@@ -272,51 +387,36 @@ class MaterialListObj {
       $(this).remove();
     });
   }
-}
 
-//服务器数据
-//function serverDataCover() {
-//  this.materialListData = {};
-//  this.materialListSubData = {};
-//  //获取仓库
-//  let warehouse_url = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehouse";
-//  let warehouse_get_warehouse = ajax_assistant(warehouse_url, "", false, true, false);
-//  //获取储罐
-//  let warehouse_pot_url = PROJECT_PATH + "lego/lego_fjTrade?servletName=getWarehousePot";
-//  let warehouse_pot_get_warehouse = ajax_assistant(warehouse_pot_url, "", false, true, false);
-////console.log(ajax_assistant);
-//  if (1 == warehouse_get_warehouse.status) {
-//    if (0 == warehouse_get_warehouse.count) {
-//      this.materialListData = {};
-//    } else {
-//      let tmp_arr = new Array();
-//      let result = JSON.parse(warehouse_get_warehouse.result);    
-//      for (let i = 0; i < result.length; i++) {
-//        // name id uuid
-//        tmp_arr[i] = {"material_name":result[i].name, "uuid":result[i].uuid};
-//      }
-//      this.materialListData["data"] = tmp_arr;
-//    }
-//    
-//  } else {
-//    alert("库区数据获取失败");
-//  }
-//  //储罐
-//  if (1 == warehouse_pot_get_warehouse.status) {
-//    if (0 == warehouse_pot_get_warehouse.count) {
-//      this.materialListSubData = {};
-//    } else {
-//      let tmp_arr_pot = new Array();
-//      let result_pot = JSON.parse(warehouse_pot_get_warehouse.result);  
-//      //console.log(result_pot);
-//      for (let i = 0; i < result_pot.length; i++) {
-//        // name id uuid
-//        tmp_arr_pot[i] = {"storage_tank_name":result_pot[i].name, "uuid":result_pot[i].uuid, "type":result_pot[i].type, "uuid":result_pot[i].uuid, "effective_capacity":result_pot[i].effective_capacity, "charge_capacity":result_pot[i].charge_capacity};
-//      }
-//      this.materialListSubData["data"] = tmp_arr_pot;
-//    }
-//    
-//  } else {
-//    alert("储罐数据获取失败");
-//  }
-//}
+  //文本输出
+  outPutTypeList(contentBoxId) {
+    let content = 
+      `<div class = "row material_type_list_row">
+         <div class = "col-lg-12 material_type_list_col">
+           <div class = "list-group">
+             <p href = "#" class = "list-group-item clearfix active">类别列表</p>
+             <div id="material_type_list_box">
+               <div>
+                 <p href = "#" class = "list-group-item clearfix material_type_list_pl30 material_type_list_bgddd">舟山纳海<span class = "glyphicon glyphicon-plus pull-right material_type_list_colorfff"></span></p>
+                 <div>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">芳烃<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">异构<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">轻油<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                 </div>
+               </div>
+               <div>
+                 <p href = "#" class = "list-group-item clearfix material_type_list_pl30 material_type_list_bgddd">舟山纳海<span class = "glyphicon glyphicon-plus pull-right material_type_list_colorfff"></span></p>
+                 <div>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">芳烃<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">异构<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                   <p href = "#" class = "list-group-item clearfix material_type_list_pl30">轻油<span class = "glyphicon glyphicon-remove pull-right material_type_list_ml15"></span><span class = "glyphicon glyphicon-pencil pull-right material_type_list_ml15"></span></p>
+                 </div>
+               </div>
+             </div>
+             <p class="list-group-item active text-center">&nbsp;</p>
+           </div>
+         </div>
+       </div>`;
+    $(contentBoxId).html(content);
+  }
+}
